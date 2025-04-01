5acf48681f0e360600e1aef30d9ec3f253d050b7
Pedro <pedrocarofene@gmail.comgit config --global user.name Pedro>
2025-04-01 00:37:13 -0300

    primeiro commit

--- projetin.py
+++ projetin.py
@@ -1,12 +1,20 @@
-print; "ola mundo" 
-
 
 print ("monyke")
 nome = "monyke thifany"
 idade = 18
-altura =1.70
+altura =1.65
+data_de_nascimento = 21/10/2006
 
 
+from datetime import datetime
 
-for i in range(5):  # De 0 a 4
-    print("monyke linda") 
+data_de_namoro = datetime(2025, 3, 22).date()
+data_atual = datetime.today().date()    
+
+data_usuario = input("Qual a data de namoro de Pedro e Monyke?")
+data_usuario = datetime.strptime(data_usuario, "%d/%m/%Y").date()
+
+if data_usuario == data_de_namoro:
+    print("Parabéns, você acertou!")
+else:
+    print("Você errou!!")
